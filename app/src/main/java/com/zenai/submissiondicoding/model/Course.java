844bf10af064475b910fdda6cdb11dfbf97d9378
Kevin <kevinrisqi22@gmail.com>
2020-04-16 22:41:36 +0700

    Add model course

---
+++ app/src/main/java/com/zenai/submissiondicoding/model/Course.java
@@ -0,0 +1,40 @@
+package com.zenai.submissiondicoding.model;
+
+public class Course {
+    private String name;
+    private String detail;
+    private String explanation;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
